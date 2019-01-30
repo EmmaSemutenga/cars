c85773596dd1f4f0578615b7fcdc427b3030ba74
Emmanuel Semutenga <emzsema@gmail.com>
2019-01-30 16:47:37 +0300

    Added portifolio page

--- bmw.py
+++ bmw.py
@@ -5,3 +5,7 @@ lexus = Flask(__name__)
 @lexus.route("/")
 def home():
     return render_template("index.html")
+
+@lexus.route("/portifolio")
+def portifolio():
+    return render_template("portifolio.html")
